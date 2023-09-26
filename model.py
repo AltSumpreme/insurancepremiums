038ad03501f0590b8613c817cd4914aa6e3f14aa
AltSumpreme <134855924+AltSumpreme@users.noreply.github.com>
2023-09-26 20:39:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 #Create inputs and targets
+non_smoker_df = medical_df[medical_df.smoker == 'no']
 inputs, targets = non_smoker_df[['age', 'bmi']], non_smoker_df['charges']
 
 # train the model
